93b974d34d0ab7dd6e85bdae25142e109f11db10
ryumakino <ricardoryu.makino@gmail.com>
2024-12-19 11:03:31 -0300

    Turbo Frames and Turbo Stream templates

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,18 +2,28 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+    @quote = quotes(:first)
   end
-
-  # ...
-  # The test we just wrote
-  # ...
 
   test "Showing a quote" do
     visit quotes_path
     click_link @quote.name
 
     assert_selector "h1", text: @quote.name
+  end
+
+  test "Creating a new quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "New quote"
+    fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
+    click_on "Create quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara quote"
   end
 
   test "Updating a quote" do
