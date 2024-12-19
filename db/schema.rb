d3abf29b80accb981bea9c7830a54adfade3351e
ryumakino <ricardoryu.makino@gmail.com>
2024-12-19 11:57:10 -0300

    Adding companies to our Rails application

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_12_16_154958) do
+ActiveRecord::Schema.define(version: 2024_12_19_145353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.string "name"
