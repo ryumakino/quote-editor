b29c06629622cbb497e8b97bffd18cd5ac0e3d61
ryumakino <ricardoryu.makino@gmail.com>
2024-12-19 16:06:50 -0300

    Another CRUD controller with Turbo Rails

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :quotes
+
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
