e801ce091b82a72daf4022dc19ce5b66bec7b89f
ryumakino <ricardoryu.makino@gmail.com>
2024-12-19 11:27:37 -0300

    Broadcasting created quotes with Turbo Streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,6 @@
 class Quote < ApplicationRecord
     validates :name, presence: true
+
+    # Broadcasting created quotes with Turbo Streams
+    after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
 end
