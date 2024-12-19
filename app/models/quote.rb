c50d0fbd6f10d2a67fd6582d398b2aca9e379454
ryumakino <ricardoryu.makino@gmail.com>
2024-12-19 11:47:20 -0300

    Real-time updates with Turbo Streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,5 @@
 class Quote < ApplicationRecord
     validates :name, presence: true
 
-    # Broadcasting created quotes with Turbo Streams
-    after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+    broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
